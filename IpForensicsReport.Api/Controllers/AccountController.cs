ba362aacb728b12fc5ffec2ffca748fbd78ae453
Vladimir Janevski <vladimir.janevski@hotmail.com>
2026-07-10 14:59:12 +0200

    Add project files.

---
+++ IpForensicsReport.Api/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IpForensicsReport.Api.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
